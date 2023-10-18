228f116d0852bd175acb3d25b4404c9ef2dc4a90
John Sloan <jsloanphoto@gmail.com>
2023-10-18 14:51:14 -0400

    naming conventions update

--- errorParser.py
+++ errorParser.py
@@ -4,15 +4,15 @@ import re
 
 # Define a function to find errors in a .tzlog file and write them to an output file
 def find_errors_in_tzlog(file_path, output_file):
-    error_pattern = r'error[:]?'
+    ERROR_PATTERN = r'error[:]?'
     # Define a pattern to skip lines containing specific messages
-    skip_pattern = r'(No (Model|Make) found in metadata|\[AIE\] WARN  Using an engine plan file across different models of devices is not recommended and is likely to affect performance or even cause errors)'
+    SKIP_PATTERN = r'(No (Model|Make) found in metadata|\[AIE\] WARN  Using an engine plan file across different models of devices is not recommended and is likely to affect performance or even cause errors)'
     
     # Open the .tzlog file for reading
     with open(file_path, 'r') as file:
         for line_number, line in enumerate(file, start=1):
-            # Check if the line contains 'error' (case-insensitive) and does not match the skip_pattern
-            if re.search(error_pattern, line, re.IGNORECASE) and not re.search(skip_pattern, line):
+            # Check if the line contains 'error' (case-insensitive) and does not match the SKIP_PATTERN
+            if re.search(ERROR_PATTERN, line, re.IGNORECASE) and not re.search(SKIP_PATTERN, line):
                 filename = os.path.basename(file_path)  # Get the filename without the path
                 error_message = line.strip()  # Remove leading/trailing whitespace from the line
                 # Write the error message along with the filename and line number to the output file
@@ -20,14 +20,14 @@ def find_errors_in_tzlog(file_path, output_file):
 
 # Define a function to process all .tzlog files in a folder
 def process_tzlog_folder(input_folder):
-    # Create an 'output_folder' named 'notes' in the same directory as the 'input_folder'
-    output_folder = os.path.join(input_folder, 'notes')
-    # Create the 'output_folder' if it doesn't exist
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+    # Create an 'OUTPUT_FOLDER' named 'notes' in the same directory as the 'input_folder'
+    OUTPUT_FOLDER = os.path.join(input_folder, 'notes')
+    # Create the 'OUTPUT_FOLDER' if it doesn't exist
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.makedirs(OUTPUT_FOLDER)
 
-    # Open 'tzNotes.txt' in the 'output_folder' for writing
-    with open(os.path.join(output_folder, 'tzNotes.txt'), 'w') as output_file:
+    # Open 'tzNotes.txt' in the 'OUTPUT_FOLDER' for writing
+    with open(os.path.join(OUTPUT_FOLDER, 'tzNotes.txt'), 'w') as output_file:
         # Recursively traverse all files in 'input_folder'
         for root, _, files in os.walk(input_folder):
             for file_name in files:
